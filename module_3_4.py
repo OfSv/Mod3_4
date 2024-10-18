3badc775f78e136eba5e142a703db669fbcb8728
OfSv <konkovocso@gmail.com>
2024-10-18 18:03:21 +0300

    Произвольное число параметров

---
+++ module_3_4.py
@@ -0,0 +1,18 @@
+# Задача "Произвольное число параметров"
+
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in range(len(other_words)):
+        if root_word.lower() in (other_words[i].lower()) or other_words[i].lower() in (root_word.lower()):
+            same_words.append(other_words[i])
+    return same_words
+
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+
+print(single_root_words('БЕДА', 'Ура! Победа!', 'Пора обедать.',
+      'У нас беда', 'пропала еда', 'Остались без обеда!'))
+print(single_root_words('ЕДА', 'Ура! Победа!', 'Пора обедать.',
+      'У нас беда - пропала еда', 'Вот это да!'))
